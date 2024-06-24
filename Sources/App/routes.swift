7eb4f81a221264f62636f131663c62db5ef67925
Sasha Jaroshevskii <jaroshevsiisasha@gmail.com>
2024-06-24 23:02:05 +0300

    Initial commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,10 @@
 import Vapor
 
+struct LoginRequest: Content {
+    let username: String
+    let password: String
+}
+
 func routes(_ app: Application) throws {
     app.get { req async in
         "It works!"
@@ -7,5 +12,14 @@ func routes(_ app: Application) throws {
 
     app.get("hello") { req async -> String in
         "Hello, world!"
+    }
+    
+    app.post("login") { req -> HTTPStatus in
+        let loginRequest = try req.content.decode(LoginRequest.self)
+        if loginRequest.username == "admin" && loginRequest.password == "admin" {
+            return .ok
+        } else {
+            return .unauthorized
+        }
     }
 }
