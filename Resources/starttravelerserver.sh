6226a331c0c7dffe23b468c5033a23d5b058aa5f
jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2024-08-20 19:37:15 +0300

    Update

---
+++ Resources/starttravelerserver.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERNAME="jaroshevskii"
+REPOSITORY="traveller-server"
+PROJECT="TravellerServer"
+EXECUTABLE="travellerserver"
+OS="Linux"
+ARCHITECTURE="x86_64"
+
+TAG=$(curl -s "https://api.github.com/repos/$USERNAME/$REPOSITORY/tags" | awk -F'"' '/"name":/ {print $4; exit}')
+VERSION=$(echo "$TAG" | sed 's/^v//' || true)
+
+ARCHIVE="$PROJECT-$VERSION-$OS-$ARCHITECTURE.zip"
+EXTRACTION_DIR="$PROJECT-$VERSION-$OS-$ARCHITECTURE"
+
+if [ -f "$ARCHIVE" ]; then
+  echo "Archive $ARCHIVE already exists. Re-downloading the archive..."
+  rm -f "$ARCHIVE"
+fi
+
+curl -LO "https://github.com/$USERNAME/$REPOSITORY/releases/download/$TAG/$ARCHIVE"
+
+if [ -d "$EXTRACTION_DIR" ]; then
+  echo "Directory $EXTRACTION_DIR already exists. Removing the old director..."
+  rm -rf "$EXTRACTION_DIR"
+fi
+
+unzip "$ARCHIVE" -d "$EXTRACTION_DIR"
+
+cd "$EXTRACTION_DIR" || exit
+
+LOG_FILE="$PROJECT.logs"
+
+if [ -f "./$EXECUTABLE" ]; then
+  echo "Starting $EXECUTABLE in the background..."
+  nohup "./$EXECUTABLE" > "$LOG_FILE" 2>&1 &
+  echo "$EXECUTABLE is running in the background. Logs are being written to $LOG_FILE."
+else
+  echo "Error: Executable $EXECUTABLE not found."
+  exit 1
+fi
+
+ngrok http --domain=evident-pug-abnormally.ngrok-free.app 8080
